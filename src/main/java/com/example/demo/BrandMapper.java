7482d8842d6c777804731a35cb28b900b6e277fa
Luca Di Carlo <luca.dicarlo.partner@decathlon.com>
2024-11-19 21:55:44 +0100

    feat: fail here

---
+++ src/main/java/com/example/demo/BrandMapper.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+@Mapper
+public interface BrandMapper {
+    @Mapping(source = "name", target = "name")
+    BrandOut map(Brand brand);
+}
